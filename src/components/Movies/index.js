cd7fb3417465949f8a35bca9a85587b6789dabf5
Derek Emsbach <demsbach92@gmail.com>
2022-09-30 01:54:56 +0000

    Update 4 files

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,22 @@
-function Movies() {
+import { NavLink, Route, Switch } from "react-router-dom";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((el) => (
+          <span key={el.id} style={{ margin: "5px" }}>
+            <NavLink to={`/movies/${el.id}`}>{el.title}</NavLink>
+          </span>
+        ))}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
     </div>
   );
 }
